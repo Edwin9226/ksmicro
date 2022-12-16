827a0f4c6b3c5ad4e894ae3863696e61924270f1
Edwin Pinchao <miguepinchao1992@gmail.com>
2022-12-16 05:07:57 -0600

    "Registering Booking service as client to Discovery service"

--- bookingservice/src/main/java/com/ks/bookingservice/BookingserviceApplication.java
+++ bookingservice/src/main/java/com/ks/bookingservice/BookingserviceApplication.java
@@ -2,8 +2,10 @@ package com.ks.bookingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BookingserviceApplication {
 
 	public static void main(String[] args) {
